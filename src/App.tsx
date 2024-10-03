2f75cba71bf751915af11f28e2d7a673b34653ec
Jeremiah Howden <jeremiahhowden@gmail.com>
2024-10-02 22:03:43 -0500

    yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
+import { AppShell, Burger, Group, MantineProvider, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Router } from './Router';
 import { theme } from './theme';
 
 export default function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <AppShell
+        layout="alt"
+        header={{ height: 60 }}
+        footer={{ height: 60 }}
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        aside={{ width: 300, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          <Group>
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <Text>Navbar</Text>
+          </Group>
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Router />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
